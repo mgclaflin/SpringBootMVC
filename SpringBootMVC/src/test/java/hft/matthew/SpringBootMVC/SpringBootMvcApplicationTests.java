37e15a3dbd99245effb96950d8304899db8c0606
mgclaflin <mgclaflin@crimson.ua.edu>
2024-12-30 17:02:27 +0100

    initial commit

---
+++ SpringBootMVC/src/test/java/hft/matthew/SpringBootMVC/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hft.matthew.SpringBootMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
